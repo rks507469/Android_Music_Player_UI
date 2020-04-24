bb375f7db04ed9e8acc41e7b9a0dfe30c2fc8571
rks507469 <44531949+rks507469@users.noreply.github.com>
2020-04-25 01:49:45 +0530

    menu added

--- app/src/main/java/com/practice/musicplayer/MainActivity.java
+++ app/src/main/java/com/practice/musicplayer/MainActivity.java
@@ -3,6 +3,11 @@ package com.practice.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar customToolbar = findViewById(R.id.customToolbar);
+        setSupportActionBar(customToolbar);
+    }
+
+    //to add menu to the app bar we need to inflate the menu first
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater customMenuInflater = getMenuInflater();
+        //if the menu does not work try to restart the android studio after clearing the cache its in the File option
+        customMenuInflater.inflate(R.menu.custom_app_bar_menu, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    //actions when the items on the menu are clicked
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                //open the search bar fragment
+                return true;
+            case R.id.action_settings:
+                //open the settings activity
+                return true;
+            case R.id.action_account:
+                //open the account activity
+                return true;
+            default:
+                //we can't recognise the user action so
+                //super class will handel it
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
