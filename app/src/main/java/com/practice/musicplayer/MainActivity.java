c836626460c877729bee18949246e7ce31f2136d
rks507469 <44531949+rks507469@users.noreply.github.com>
2020-04-26 18:41:12 +0530

    working with the fragments

--- app/src/main/java/com/practice/musicplayer/MainActivity.java
+++ app/src/main/java/com/practice/musicplayer/MainActivity.java
@@ -1,29 +1,48 @@
 package com.practice.musicplayer;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //code for the things are populated when the activity is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //creating teh tool bar
         Toolbar customToolbar = findViewById(R.id.customToolbar);
         setSupportActionBar(customToolbar);
+
+        //creating the fragments view
+        //checking for the fragment container
+        if(findViewById(R.id.fragment_container) != null) {
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+            //creating the fragment
+            LibraryFragment libraryFragment = new LibraryFragment();
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            libraryFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, libraryFragment).commit();
+        }
     }
 
     //to add menu to the app bar we need to inflate the menu first
@@ -56,20 +75,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Bottom navigation menu actions on the selection of the items
+    //it will be used to trigger the functions to call the fragments
+    //to load the fragments in the activity
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             Fragment fragment;
             switch (menuItem.getItemId()) {
                 case R.id.navigation_library:
+                    //Library fragment
+                    LibraryFragment libraryFragment = new LibraryFragment();
+                    FragmentTransaction libraryFragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    libraryFragmentTransaction.replace(R.id.fragment_container, libraryFragment);
+                    libraryFragmentTransaction.addToBackStack(null);
+                    libraryFragmentTransaction.commit();
                     return true;
                 case R.id.navigation_for_you:
+                    //ForYou fragment
+                    ForYouFragment forYouFragment = new ForYouFragment();
+                    FragmentTransaction forYouFragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    forYouFragmentTransaction.replace(R.id.fragment_container, forYouFragment);
+                    forYouFragmentTransaction.addToBackStack(null);
+                    forYouFragmentTransaction.commit();
                     return true;
                 case R.id.navigation_browse:
+                    //ignore this for the moment
                     return  true;
                 case R.id.navigation_radio:
+                    //ignore this for the moment
                     return  true;
                 default:
+                    //ignore this for the moment
                     return false;
             }
         }
